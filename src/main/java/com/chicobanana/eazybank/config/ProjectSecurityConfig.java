93fd9152cc738578855c2f0132fa3c54ce479bc5
Cary Guca <cary.guca@gmail.com>
2023-11-21 14:02:19 -0600

    Added BCrypt encoding for password

--- src/main/java/com/chicobanana/eazybank/config/ProjectSecurityConfig.java
+++ src/main/java/com/chicobanana/eazybank/config/ProjectSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configurers.AuthorizeH
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -87,8 +88,10 @@ public class ProjectSecurityConfig {
 //        return new JdbcUserDetailsManager(dataSource);
 //    }
 
+//
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
